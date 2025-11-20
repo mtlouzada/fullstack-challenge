dd58cd6c669b9d66e00a055beac607863d6ae85a
mtlouzada <matheuslouzadaa@gmail.com>
2025-11-19 21:57:45 -0300

    feat: aplicando mais robustez a logica

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+  console.log(`Auth service listening on ${port}`);
 }
 bootstrap();
