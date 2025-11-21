0b15512a11b41bf9eb14bad62724ea63b08bd3dd
mtlouzada <matheuslouzadaa@gmail.com>
2025-11-20 21:42:03 -0300

    refactor: reajustando syntax que estava invertida

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,21 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AuthModule } from './auth/auth.module';
+
+declare const process: { env: { [key: string]: string | undefined } };
 
 @Module({
   imports: [
     AuthModule,
-    ClientsModule.register([
-      {
-        name: 'AUTH_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: [process.env.RABBITMQ_URL || 'amqp://guest:guest@rabbitmq:5672'],
-          queue: 'auth_queue',
-          queueOptions: { durable: false },
-        },
-      },
-    ]),
+
   ],
 })
 export class AppModule {}
--- apps/api-gateway/src/auth/auth.module.ts
+++ apps/api-gateway/src/auth/auth.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 @Module({
-  controllers: [AuthController],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'AUTH_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://rabbitmq:5672'],
+          queue: 'auth_queue',
+          queueOptions: { durable: true },
+        },
+      },
+    ]),
+  ],
   providers: [AuthService],
+  controllers: [AuthController],
+  exports: [AuthService, ClientsModule],
 })
 export class AuthModule {}
