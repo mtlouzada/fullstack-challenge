4c2607fc8fe3c468d7d03a0ff1e526663dbce01a
mtlouzada <matheuslouzadaa@gmail.com>
2025-11-23 16:41:29 -0300

    feat: adicionando camada para filtrar error 500 no login

--- apps/api-gateway/src/auth/auth.service.ts
+++ apps/api-gateway/src/auth/auth.service.ts
@@ -1,28 +1,34 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom, timeout } from 'rxjs';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @Inject('AUTH_SERVICE') private readonly authClient: ClientProxy,
-  ) {}
+constructor(
+@Inject('AUTH_SERVICE') private readonly authClient: ClientProxy,
+) {}
 
-  private async send(pattern: string, data: any) {
+private async send(pattern: string, data: any) {
+  try {
     await this.authClient.connect();
     const obs = this.authClient.send(pattern, data);
-    return firstValueFrom(obs.pipe(timeout({ each: 8000 })));
+    return await firstValueFrom(obs.pipe(timeout({ each: 8000 })));
+  } catch (err) {
+    console.error('🔥 ERRO NO AUTH MICROSERVICE:', err);
+    throw err;
   }
+}
 
-  login(data: any) {
-    return this.send('auth.login', data);
-  }
 
-  register(data: any) {
-    return this.send('auth.register', data);
-  }
+login(data: any) {
+return this.send('auth.login', data);
+}
 
-  refresh(refreshToken: string) {
-    return this.send('auth.refresh', { refreshToken });
-  }
+register(data: any) {
+return this.send('auth.register', data);
+}
+
+refresh(refreshToken: string) {
+return this.send('auth.refresh', { refreshToken });
+}
 }
