532a2f8450ba877d7739e2ed618c88de36d4ae18
mtlouzada <matheuslouzadaa@gmail.com>
2025-11-22 22:35:55 -0300

    feat: corrigindo path e portas

--- apps/auth-service/data-source.ts
+++ apps/auth-service/data-source.ts
@@ -1,17 +1,18 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import { User } from './src/entities/user.entity';
+import * as path from 'path';
 import * as dotenv from 'dotenv';
-dotenv.config();
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export default new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASS || 'postgres',
-  database: process.env.DB_NAME || 'auth_db',
-  entities: [User],
-  migrations: [__dirname + '/../migrations/*.{ts,js}'],
-  synchronize: false
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [path.join(__dirname, 'src/**/*.entity.{ts,js}')],
+  migrations: [path.join(__dirname, 'migrations/*.{ts,js}')],
+  synchronize: false,
 });
